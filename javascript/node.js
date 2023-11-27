5ad7ec12991d20771663854bdd09f0f496928632
UrWorstNightmar3 <l.khosbileg0918@gmail.com>
2023-11-27 23:25:47 +0800

    gerees

--- javascript/node.js
+++ javascript/node.js
@@ -1,18 +1,28 @@
 //arithmetic oparetion
 
-let day = 10;
-if (day == 1) {
-  console.log("monday");
-} else if (day == 2) {
-  console.log("tuesday");
-} else if (day == 3) {
-  console.log("wednesday");
-} else if (day == 4) {
-  console.log("thursday");
-} else if (day == 5) {
-  console.log("friday");
-} else if (day == 6) {
-  console.log("saturday");
-} else {
-  console.log("sunday");
+let days = 255;
+if (days <= 31) {
+  console.log("January");
+} else if (days <= 59) {
+  console.log("Febraury");
+} else if (days <= 90) {
+  console.log("March");
+} else if (days <= 120) {
+  console.log("April");
+} else if (days <= 151) {
+  console.log("May");
+} else if (days <= 181) {
+  console.log("June");
+} else if (days <= 212) {
+  console.log("July");
+} else if (days <= 243) {
+  console.log("August");
+} else if (days <= 273) {
+  console.log("September");
+} else if (days <= 304) {
+  console.log("October");
+} else if (days <= 334) {
+  console.log("November");
+} else if (days <= 365) {
+  console.log("December");
 }
