96e3ceabcd12abab0f16063e5112ce49f6ac03bc
UrWorstNightmar3 <l.khosbileg0918@gmail.com>
2023-11-27 12:31:08 +0800

    angias

---
+++ javascript/node.js
@@ -0,0 +1,18 @@
+//arithmetic oparetion
+
+let day = 10;
+if (day == 1) {
+  console.log("monday");
+} else if (day == 2) {
+  console.log("tuesday");
+} else if (day == 3) {
+  console.log("wednesday");
+} else if (day == 4) {
+  console.log("thursday");
+} else if (day == 5) {
+  console.log("friday");
+} else if (day == 6) {
+  console.log("saturday");
+} else {
+  console.log("sunday");
+}
