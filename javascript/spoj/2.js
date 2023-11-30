2f2f0dd33110b8172a58a0ead8e2dcb91d9b29e1
UrWorstNightmar3 <khosoo.fsdev@gmail.com>
2023-11-30 12:30:13 +0800

    angias

---
+++ javascript/spoj/2.js
@@ -0,0 +1,25 @@
+// salary ni 2 sayaas ih, 
+// niigmiin daatgaliin shimtgel toldog bh, 
+// passporttoi eswel identitycardtai bh, 
+// bas person bh eswel itgemjleheer bh, 
+// ept ni 1 sayas baga bwal zeel garna.
+ 
+
+function loan(salary,){
+if (salary >= 20000000,
+NDSH == "always",
+passport == 1 ,identification == 1,
+partner==1, itgemjleh==1, person==1,
+existingloan>=1000000) 
+{return "Approved"}
+else {return "Denied"}
+}  
+let loanApp = loan (
+    salary = 20000000,
+    NDSH = "always",
+    passport = 1,
+    partner = 1,
+    existingloan = 500000
+)
+console.log(loanApp)
+
---
+++ javascript/spoj/city.js
@@ -0,0 +1,43 @@
+// let city = ['Ulaanbaatar','Dornod','Sukhbaatar','Dornogovi','Omnogovi','Baynkhongor','Govi-altai','Hovd'];
+// console.log(city[0]);
+// console.log(city[1]);
+// console.log(city[2]);
+// console.log(city[3]);
+// console.log(city[4]);
+// console.log(city[5]);
+// console.log(city[6]);
+// console.log(city[7]);
+
+
+
+let usa = {
+  California :{
+    population: 39237836},
+  Texas:{
+    population:29527941},
+  Florida :{
+    population:21781128},
+  NewYork:{
+    population:19835913},
+  Pennsylvania:{
+    population:12964056},
+  Illinois:{
+    population:12671469},
+  Ohio:{
+    population:11780017},
+  Georgia:{
+    population:10799566},
+  NorthCarolina:{
+    population:10551162},
+  Michigan:{
+    population:10050811},
+  NewJersey :{
+    population:9267130},
+  Virginia: {
+    population:8642274},
+  Washington:{
+    population:7738692},
+  Arizona:{
+    population : 7276316}
+}
+console.log(usa)
