59331c453c954cdedcf2efc2b909c8174706b7e6
UrWorstNightmar3 <khosoo.fsdev@gmail.com>
2023-12-19 09:09:00 +0800

    adda

--- java.dom-exe/random/script.js
+++ java.dom-exe/random/script.js
@@ -1,13 +1,13 @@
 let num = (Math.round(Math.random() * 100))
 console.log(num)
-function check(guess) {
+function check() {
     let guess = document.getElementById('number').innerText
     guess = Number(guess)
     if (guess < num){return document.getElementById('text').innerText ='too low'}
-    else if (guess > num) {return document.getElementById('text').innerText ='too low'}
+    else if (guess > num) {return document.getElementById('text').innerText ='too high'}
     else if (guess == num) return document.getElementById('answer').innerText ="Correct!!!";
     else return document.getElementById('text').innerText ='not a number'
 }
 function reset(){  
-let num = (Math.round(Math.random() * 100))
+
 }
