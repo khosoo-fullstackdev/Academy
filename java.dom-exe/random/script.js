4cb27f7dda0b1e1c46817153d787ca925d422c6b
UrWorstNightmar3 <khosoo.fsdev@gmail.com>
2023-12-19 09:09:17 +0800

    fsefse

--- java.dom-exe/random/script.js
+++ java.dom-exe/random/script.js
@@ -1,13 +1,12 @@
-let num = (Math.round(Math.random() * 100))
+let num = Math.round(Math.random() * 100);
 console.log(num)
 function check() {
-    let guess = document.getElementById('number').innerText
-    guess = Number(guess)
-    if (guess < num){return document.getElementById('text').innerText ='too low'}
-    else if (guess > num) {return document.getElementById('text').innerText ='too high'}
-    else if (guess == num) return document.getElementById('answer').innerText ="Correct!!!";
+    let guess = Number(document.getElementById('number').value)
+    if (guess < num) {document.getElementById("text").innerText = 'too low'}
+    else if (guess > num) {document.getElementById("text").innerText = 'too high'}
+    else if (guess == num) {document.getElementById('text').innerText = "Correct!!!"}
     else return document.getElementById('text').innerText ='not a number'
 }
-function reset(){  
-
+function reset(){ 
+     
 }
